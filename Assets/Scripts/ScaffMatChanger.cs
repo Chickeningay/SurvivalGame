fc646d38534fbdced4380563d6b9e3aa0e0bb389
Chickeningay <bjkygz@gmail.com>
2021-09-20 19:31:54 +0300

    asd

--- Assets/Scripts/ScaffMatChanger.cs
+++ Assets/Scripts/ScaffMatChanger.cs
@@ -7,6 +7,7 @@ public class ScaffMatChanger : MonoBehaviour
     public string currentMat;
     public Material WoodMat;
     public Material GlassMat;
+    bool stopprocess;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,35 +17,51 @@ public class ScaffMatChanger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.GetComponent<HitDetection>().hit)
-        {
-            gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
-            currentMat = "";
-            gameObject.transform.GetChild(1).gameObject.GetComponent<ParticleSystem>().playOnAwake = true;
-        }
-        if (currentMat != "")
-        {
-            if (currentMat == "Wood")
+        if(!stopprocess) {
+            if (gameObject.GetComponent<HitDetection>().hit)
             {
-                gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
-                gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material = WoodMat;
-                gameObject.transform.GetChild(1).GetComponent<ParticleSystemRenderer>().material = WoodMat;
+                gameObject.GetComponent<HitDetection>().hit = false;
+                gameObject.GetComponent<HitDetection>().enabled = false;
+                gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
+                gameObject.transform.GetChild(0).GetComponent<BoxCollider>().enabled = false;
                 gameObject.GetComponent<MeshRenderer>().enabled = false;
-            }
-            else if (currentMat == "Glass")
-            {
-                gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
-                gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material = GlassMat;
-                gameObject.transform.GetChild(1).GetComponent<ParticleSystemRenderer>().material = GlassMat;
-                gameObject.GetComponent<MeshRenderer>().enabled = false;
+                gameObject.GetComponent<BoxCollider>().enabled = false;
 
+                currentMat = "";
+                gameObject.transform.GetChild(1).gameObject.GetComponent<ParticleSystem>().Play();
+                StartCoroutine(StartDeletion());
+                stopprocess = true;
+            }
+            else if (currentMat != "")
+            {
+                if (currentMat == "Wood")
+                {
+                    gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
+                    gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material = WoodMat;
+                    gameObject.transform.GetChild(1).GetComponent<ParticleSystemRenderer>().material = WoodMat;
+                    gameObject.GetComponent<MeshRenderer>().enabled = false;
+                }
+                else if (currentMat == "Glass")
+                {
+                    gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
+                    gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material = GlassMat;
+                    gameObject.transform.GetChild(1).GetComponent<ParticleSystemRenderer>().material = GlassMat;
+                    gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+                }
+            }
+            else if (currentMat == "" && !gameObject.GetComponent<HitDetection>().hit)
+            {
+                gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
+                gameObject.GetComponent<MeshRenderer>().enabled = true;
             }
         }
-        else
-        {
-            gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
-            gameObject.GetComponent<MeshRenderer>().enabled = true;
-        }
+        
+    }
+    IEnumerator StartDeletion()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
     }
 
 }
