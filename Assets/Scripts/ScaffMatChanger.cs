706e6e481da514dc0365cf4c26995aa83e730a95
Chickeningay <bjkygz@gmail.com>
2021-09-20 17:21:29 +0300

    breakable buildings

--- Assets/Scripts/ScaffMatChanger.cs
+++ Assets/Scripts/ScaffMatChanger.cs
@@ -16,18 +16,26 @@ public class ScaffMatChanger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (gameObject.GetComponent<HitDetection>().hit)
+        {
+            gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
+            currentMat = "";
+            gameObject.transform.GetChild(1).gameObject.GetComponent<ParticleSystem>().playOnAwake = true;
+        }
         if (currentMat != "")
         {
             if (currentMat == "Wood")
             {
                 gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
                 gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material = WoodMat;
+                gameObject.transform.GetChild(1).GetComponent<ParticleSystemRenderer>().material = WoodMat;
                 gameObject.GetComponent<MeshRenderer>().enabled = false;
             }
             else if (currentMat == "Glass")
             {
                 gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
                 gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material = GlassMat;
+                gameObject.transform.GetChild(1).GetComponent<ParticleSystemRenderer>().material = GlassMat;
                 gameObject.GetComponent<MeshRenderer>().enabled = false;
 
             }
