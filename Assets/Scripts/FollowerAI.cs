f9c2581d4dbb4e783f5b22ca84e5774635efc5f8
Chickeningay <bjkygz@gmail.com>
2021-09-22 01:55:12 +0300

    FoxAnimComplete

--- Assets/Scripts/FollowerAI.cs
+++ Assets/Scripts/FollowerAI.cs
@@ -8,10 +8,12 @@ public class FollowerAI : MonoBehaviour
     bool AIenabled;
     public GameObject Player;
     public AnimationClip MovementClip;
+    public GameObject Head;
+    Vector3 startrot;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startrot = gameObject.transform.GetChild(1).transform.eulerAngles;
     }
 
     // Update is called once per frame
@@ -22,14 +24,16 @@ public class FollowerAI : MonoBehaviour
         if (AIenabled)
         {
             gameObject.GetComponent<NavMeshAgent>().destination = Player.transform.position;
-            gameObject.transform.GetChild(1).transform.LookAt(Player.transform);
+            //Head.transform.LookAt(Player.transform);
+
+
+            // gameObject.transform.GetChild(1).transform.eulerAngles = new Vector3(0, startrot.y+90, 0);
+            Vector3 lTargetDir = Player.transform.position - gameObject.transform.GetChild(1).transform.position;
+            lTargetDir.y = 0.0f;
+            gameObject.transform.GetChild(1).transform.rotation = Quaternion.RotateTowards(new Quaternion(gameObject.transform.GetChild(1).transform.rotation.x, gameObject.transform.GetChild(1).transform.rotation.y, gameObject.transform.GetChild(1).transform.rotation.z, gameObject.transform.GetChild(1).transform.rotation.w), Quaternion.LookRotation(new Vector3(lTargetDir.x, lTargetDir.y, lTargetDir.z )), Time.time * 2);
+            Head.GetComponent<Animator>().enabled = true;
+            Head.GetComponent<Animator>().Play(MovementClip.name);
             
-           
-           gameObject.transform.GetChild(1).transform.eulerAngles = new Vector3(0, gameObject.transform.GetChild(1).transform.eulerAngles.y+90, 0);
-            gameObject.transform.GetChild(1).GetComponent<Animator>().enabled = true;
-            gameObject.transform.GetChild(1).GetComponent<Animator>().Play(MovementClip.name);
-
-
 
         }
     }
